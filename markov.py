32d497720bf4ef3ee4f5d27d51c583895fd4cf6c
vagrant <vagrant@vagrant.vm>
2020-05-07 00:31:28 +0000

    Added MarkovBot to markov.py

--- markov.py
+++ markov.py
@@ -3,6 +3,9 @@
 import sys
 from random import choice
 
+import os
+import discord
+
 
 def open_and_read_file(filenames):
     """Take list of files. Open them, read them, and return one long string."""
@@ -64,3 +67,21 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}.format(client')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content:
+        await message.channel.send(make_text(chains))
+
+client.run(os.environ['DISCORD_TOKEN'])
